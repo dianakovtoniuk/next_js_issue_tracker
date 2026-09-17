26a2df828df674450d2eeea1575c6b8c050bb1e2
KovtoniukDiana <buben111004@gmail.com>
2026-09-17 15:54:58 +0300

    prisma migration

---
+++ app/components/issueStatusBadge.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Status, Issue } from '@/app/generated/prisma';
+
+interface IProps {
+    status: Status;
+} 
+
+function IssueStatusBadge() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default IssueStatusBadge
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {Theme} from "@radix-ui/themes";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/navBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
