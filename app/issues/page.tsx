de05cba7b7e6e9a810f9893ffe817791b8bd8fe8
KovtoniukDiana <buben111004@gmail.com>
2026-09-17 12:41:32 +0300

    Radix UI set up

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import {Button} from "@radix-ui/themes";
+
 
 function IssuesPage() {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Theme} from "@radix-ui/themes";
 import NavBar from "./components/NavBar";
 
 const geistSans = Geist({
@@ -25,12 +27,12 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        
-        <NavBar />
-        <main>
-          {children}
-        </main>
-        
+        <Theme>
+          <NavBar />
+          <main>
+            {children}
+          </main>
+        </Theme>
       </body>
     </html>
   );
